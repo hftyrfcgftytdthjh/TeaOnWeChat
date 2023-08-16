e20ba6eb34d5ea55f41f711d1b7cf52a530f4c36
hftyrfcgftytdthjh <815723470@qq.com>
2023-08-16 15:42:17 +0800

    first commit

---
+++ components/xzs-news-item/xzs-news-item.js
@@ -0,0 +1,33 @@
+// components/xzs-news-item/xzs-news-item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:""
+    },
+    item:{
+      type:Object,
+      value:{
+        title:"moren",
+        author:"hejing"
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
